8c9aef70d15d3abddac8be5d8c8d584476dccb77
Mfofana <mamoutou1992fofana@gmail.com>
2023-02-21 10:31:41 +0100

    completed data ingestion script

--- ingestion.py
+++ ingestion.py
@@ -22,8 +22,8 @@ today = datetime.today().strftime('%Y%m%d')
 #############Function for data ingestion
 def merge_multiple_dataframe():
     """
-
-    :return:
+    Combine the data in all of individual datasets into a single DataFrame and save on the disk.
+    :return: Single DataFrame recorded and save on the disk
     """
     final_df = pd.DataFrame(
             columns=['corporation',
@@ -34,19 +34,26 @@ def merge_multiple_dataframe():
     )
     # list of files
     filenames = os.listdir(input_folder_path)
-    # path to store the final data
-    final_data = os.path.join(output_folder_path, f'{today}_final_data.csv')
-
+    # list to content individual file
+    ingested_files = []
     # check for datasets, compile them together, and write to an output file
     for each_filename in filenames:
         each_filename_path = os.path.join(input_folder_path, each_filename)
         current_df = pd.read_csv(each_filename_path)
         final_df = pd.concat([final_df, current_df], axis=0)
+        ingested_files.append(each_filename)
 
     # drop duplicates
     result = final_df.drop_duplicates()
+    # path to store the final data
+    data_path = os.path.join(output_folder_path, f'{today}_final_data.csv')
     # save the final data in csv
-    result.to_csv(final_data, index=False)
+    result.to_csv(data_path, index=False)
+
+    data_path = os.path.join(output_folder_path, f'{today}_ingestedfiles.txt')
+    # list of the ingested .csv files
+    with open(data_path, 'w') as f:
+        f.write(str(ingested_files))
 
 if __name__ == '__main__':
     merge_multiple_dataframe()
