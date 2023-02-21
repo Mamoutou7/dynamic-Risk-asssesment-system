0f7842239b99f4f4be761ffa58f6f5e00d2dd2b7
Mfofana <mamoutou1992fofana@gmail.com>
2023-02-21 13:36:40 +0100

    completed model deploument script

--- deployment.py
+++ deployment.py
@@ -1,11 +1,9 @@
-from flask import Flask, session, jsonify, request
+import shutil
+
 import pandas as pd
 import numpy as np
 import pickle
 import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 import json
 
 
@@ -16,12 +14,18 @@ with open('config.json','r') as f:
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
 prod_deployment_path = os.path.join(config['prod_deployment_path']) 
-
+model_path = os.path.join(config['output_model_path'])
 
 ####################function for deployment
-def store_model_into_pickle(model):
-    #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
-        
-        
-        
+def store_model_into_pickle():
+    # copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
+    files_to_copy = ['latestscore.txt', 'trainedmodel.pkl']
+    for file in files_to_copy:
+        shutil.copy(os.path.join(model_path, file), os.path.join(prod_deployment_path, file))
 
+        
+    # copy ingestion data file (ingestfiles.txt)
+    shutil.copy(os.path.join(dataset_csv_path, 'ingestedfiles.txt'), os.path.join(prod_deployment_path, 'ingestedfiles.txt'))
+
+if __name__ == '__main__':
+    store_model_into_pickle()
